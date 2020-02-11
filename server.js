f33fda44b193c0f02192f76ff9a8b79f0455b8be
Victor Corassa Pinheiro <victorcorassa@gmail.com>
2020-02-11 18:04:30 -0300

    Server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const debug = require("debug")("nodestr:server");
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = searchPort(process.env.PORT || 3000);
 app.set("port", port);
 
 const server = http.createServer(app);
@@ -21,3 +21,16 @@ app.use("/", route);
 
 server.listen(port);
 console.log("API FUNCIONANDO NA PORTA: " + port);
+
+function searchPort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
